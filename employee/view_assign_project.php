0e2afe7929d5cd9a01b6edc9cd6271074abb3d7b
Zunaira6 <88235468+Zunaira6@users.noreply.github.com>
2021-08-03 22:55:13 +0500

    Update view_assign_project.php

--- employee/view_assign_project.php
+++ employee/view_assign_project.php
@@ -19,7 +19,7 @@ include '../db_connection.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Assign Project |EMS</title>
 
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.13/css/jquery.dataTables.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
