60e157401465d6bb1d70f30bcffc5e750d82c4c2
Zunaira6 <88235468+Zunaira6@users.noreply.github.com>
2021-08-03 22:52:57 +0500

    Update employee_dashboard.php

--- employee/employee_dashboard.php
+++ employee/employee_dashboard.php
@@ -12,7 +12,7 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin']!=true){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dashboard |EMS</title>
     <link rel="stylesheet" href="employee_dashboard.css">
     <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
     
