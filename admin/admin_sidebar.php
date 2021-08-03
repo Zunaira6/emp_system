51570a87e2ba46fb3a2c5ce4d1a163cb192c1761
Zunaira6 <88235468+Zunaira6@users.noreply.github.com>
2021-08-03 17:30:50 +0500

    Update admin_sidebar.php

--- admin/admin_sidebar.php
+++ admin/admin_sidebar.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="admin_sidebar.css">
     <link rel="stylesheet" href="admin_dashboard.css">
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
    </head>
 <body>
