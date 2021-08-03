09e24fc8f5a6e1579d63aca0ce0c5cc84f4b09ea
Zunaira6 <88235468+Zunaira6@users.noreply.github.com>
2021-08-03 17:31:10 +0500

    Update employee_sidebar.php

--- employee/employee_sidebar.php
+++ employee/employee_sidebar.php
@@ -5,6 +5,7 @@
 
     <link rel="stylesheet" href="employee_sidebar.css">
     <link rel="stylesheet" href="employee_dashboard.css">
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
    </head>
 <body>
